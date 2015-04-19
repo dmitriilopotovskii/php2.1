a5a28b5a5aebc590deaf20ffe77e88b84a3dfbc2
dmitriilopotovskii <lom1666@gmail.com>
2015-04-20 01:38:19 +0300

    addform

--- views/news/all.php
+++ views/news/all.php
@@ -26,7 +26,7 @@
 
         <div class="col-lg-12">
             <h1 class="page-header"></h1>
-            <?php require __DIR__ . '/AddForm.php'; ?>
+            <?php require __DIR__ . '/addform.php'; ?>
         </div>
         <?php foreach ($allNews as $item) : ?>
             <div class="col-lg-12 col-md-12 col-xs-12 thumb">
