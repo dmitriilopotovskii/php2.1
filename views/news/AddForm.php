101f4d03bc6b3775acc5fa39ac604e255d2fe14f
dmitriilopotovskii <lom1666@gmail.com>
2015-04-20 01:32:00 +0300

    addform

---
+++ views/news/AddForm.php
@@ -0,0 +1,9 @@
+<form class="form-inline" enctype="multipart/form-data" action="/index.php?admin" method="POST"
+      style="margin-left: 50%;">
+
+    <input type="file" name="userfile" class="btn btn-primary "/>
+    <input type="text" class="form-control" name="title" placeholder="введите имя novosti">
+<!--    <input type="text" class="form-control" name="text" placeholder="введите text">-->
+    <p><textarea rows="10" cols="45" name="text" placeholder="введите text"></textarea></p>
+    <input class="btn btn-primary" type="submit"/>
+</form>
